8ed57813ec8f14cc5c6566f77ff5947bfe624eef
Bhargav S <51982154+bhargav-s-271100@users.noreply.github.com>
2019-06-27 21:45:00 +0530

    Add files via upload

---
+++ patterns using FOR loops.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+main()
+{
+	using namespace std;
+	for(int i=0;i<=5;i++)
+	{
+		cout<<"\n";
+		for(int j=0;j<i;j++)
+		  cout<<i;
+	}
+    cout<<"\n";
+	for(int i=0;i<=5;i++)
+    {
+		cout<<"\n";
+		for(int j=0;j<i;j++)
+		  cout<<j;
+	}
+	cout<<"\n";
+	for(int i=0;i<=5;i++)
+	{
+		cout<<"\n";
+		for(int j=0;j<=5;j++)
+		  cout<<i;
+	}
+}
+
