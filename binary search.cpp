8561edbb812b3fe852055bdcc5065394b81a8366
Bhargav S <51982154+bhargav-s-271100@users.noreply.github.com>
2019-08-08 06:43:04 +0530

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,30 @@
+#include<iostream.>
+main()
+{
+	
+	using namespace std;
+	int i,a[100],beg=0,mid,end,n,num;
+	cout<<"enter the number of elements in the array";
+	cin>>n;
+	cout<<"enter the array";
+	for(i=0;i<n;i++)
+	   cin>>a[i];
+	cout<<"enter the number to be searched";
+	cin>>num;
+	end=n-1;
+	while(beg<=end)
+	   {
+	   	mid=(beg+end)/2;
+	   	if(a[mid]>=num)
+	   	 {
+			cout<<"the required term is found at position"<< mid+1;
+	   	    break;
+	   	 }
+		   else if(a[mid]>num)
+	   	  end=mid-1;
+		else
+		   beg=mid+1; 
+		
+	   }
+	 return 0;     
+}
